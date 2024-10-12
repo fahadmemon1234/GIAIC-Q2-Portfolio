c05da1f80dbf1ec2660323a86efdc5c2a42ed094
fahadmemon1234 <fahadmemon131@gmail.com>
2024-10-12 17:22:15 +0500

    add file

--- src/app/Component/Home/page.tsx
+++ src/app/Component/Home/page.tsx
@@ -29,12 +29,8 @@ const Main = () => {
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
-              className="object-cover object-center rounded w-4/6 m-auto"
-              alt=""
-              src=""
-            />
             <Image
+              className="object-cover object-center rounded w-4/6 m-auto"
               src="/assets/img/home.png"
               alt="home"
               width={400}
