6ac83b3212ed9047fc747af3f211fd3293f90306
fahadmemon1234 <fahadmemon131@gmail.com>
2024-10-19 20:06:45 +0500

    add file

--- src/app/Component/Home/page.tsx
+++ src/app/Component/Home/page.tsx
@@ -115,7 +115,7 @@ const Main = () => {
           <div className="grid grid-rows-1 lg:grid-cols-12 md:grid-cols-4 grid-cols-12 items-center pb-10">
             <div className="lg:text-left lg:col-span-4 md:col-span-6 sm:col-span-12 col-span-12">
               <Image
-                src={"/assets/img/myPic.png"}
+                src={"/assets/img/logo.png"}
                 width={246}
                 className="rounded !w-[246px] !h-[180px] object-cover lg:mx-auto"
                 height={180}
@@ -124,23 +124,23 @@ const Main = () => {
             </div>
             <div className="lg:text-left lg:col-span-8 md:col-span-8 sm:col-span-12 col-span-12 lg:pt-0 md:pt-0 md:pl-0 sm:pl-0 pt-5">
               <h4 className="font-bold lg:text-[30px] md:text-[30px] mb-5 text-[20px]">
-                Designing Dashboards
+                Logo Designing
               </h4>
 
               <div className="grid grid-cols-12 items-center">
                 <span className="lg:col-span-2 md:col-span-2 sm:col-span-2 col-span-3 text-center bg-[#142850] text-white text-[18px] font-black me-2 px-3 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                  2020
+                  2022
                 </span>
 
                 <p className="pl-4 lg:col-span-10 md:col-span-10 sm:col-span-10 col-span-9 text-[#8695A4] text-[20px]">
-                  Dashboard
+                  logo design
                 </p>
               </div>
 
               <p className="pt-5 text-[16px] text-[#21243D]">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequat duis enim velit mollit.
-                Exercitation veniam consequat sunt nostrud amet.
+                From beauty salons to restaurants to real estate, we design
+                logos across industries. Our client projects focus on standout
+                brand identities that fit the needs of each business.
               </p>
             </div>
           </div>
@@ -149,7 +149,7 @@ const Main = () => {
           <div className="pt-10 grid grid-rows-1 lg:grid-cols-12 md:grid-cols-4 grid-cols-12 items-center pb-10">
             <div className="lg:text-left lg:col-span-4 md:col-span-6 sm:col-span-12 col-span-12">
               <Image
-                src={"/assets/img/myPic.png"}
+                src={"/assets/img/Donation.jpg"}
                 width={246}
                 className="rounded !w-[246px] !h-[180px] object-cover lg:mx-auto"
                 height={180}
@@ -158,23 +158,22 @@ const Main = () => {
             </div>
             <div className="lg:text-left lg:col-span-8 md:col-span-8 sm:col-span-12 col-span-12 lg:pt-0 md:pt-0 md:pl-0 sm:pl-0 pt-5">
               <h4 className="font-bold lg:text-[30px] md:text-[30px] mb-5 text-[20px]">
-                Designing Dashboards
+                Donation-Application
               </h4>
 
               <div className="grid grid-cols-12 items-center">
                 <span className="lg:col-span-2 md:col-span-2 sm:col-span-2 col-span-3 text-center bg-[#142850] text-white text-[18px] font-black me-2 px-3 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                  2020
+                  2023
                 </span>
 
                 <p className="pl-4 lg:col-span-10 md:col-span-10 sm:col-span-10 col-span-9 text-[#8695A4] text-[20px]">
-                  Dashboard
+                  Admin-Portal
                 </p>
               </div>
 
               <p className="pt-5 text-[16px] text-[#21243D]">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequat duis enim velit mollit.
-                Exercitation veniam consequat sunt nostrud amet.
+                Building a Donation Admin Portal to streamline donation
+                management and enhance learning through hands-on development.
               </p>
             </div>
           </div>
@@ -183,7 +182,7 @@ const Main = () => {
           <div className="pt-10 grid grid-rows-1 lg:grid-cols-12 md:grid-cols-4 grid-cols-12 items-center pb-10">
             <div className="lg:text-left lg:col-span-4 md:col-span-6 sm:col-span-12 col-span-12">
               <Image
-                src={"/assets/img/myPic.png"}
+                src={"/assets/img/website.png"}
                 width={246}
                 className="rounded !w-[246px] !h-[180px] object-cover lg:mx-auto"
                 height={180}
@@ -192,23 +191,23 @@ const Main = () => {
             </div>
             <div className="lg:text-left lg:col-span-8 md:col-span-8 sm:col-span-12 col-span-12 lg:pt-0 md:pt-0 md:pl-0 sm:pl-0 pt-5">
               <h4 className="font-bold lg:text-[30px] md:text-[30px] mb-5 text-[20px]">
-                Designing Dashboards
+                BarBer Store
               </h4>
 
               <div className="grid grid-cols-12 items-center">
                 <span className="lg:col-span-2 md:col-span-2 sm:col-span-2 col-span-3 text-center bg-[#142850] text-white text-[18px] font-black me-2 px-3 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                  2020
+                  2022
                 </span>
 
                 <p className="pl-4 lg:col-span-10 md:col-span-10 sm:col-span-10 col-span-9 text-[#8695A4] text-[20px]">
-                  Dashboard
+                  website design
                 </p>
               </div>
 
               <p className="pt-5 text-[16px] text-[#21243D]">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequat duis enim velit mollit.
-                Exercitation veniam consequat sunt nostrud amet.
+                Sleek barber store website with online booking, service details,
+                and stylist profiles. Mobile-friendly with easy navigation and
+                integrated payments for a seamless user experience.
               </p>
             </div>
           </div>
