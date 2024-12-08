2748b55ea4c32cfbd10939ee10eb6421eaa4bda3
fahadmemon1234 <fahadmemon131@gmail.com>
2024-12-09 00:16:48 +0500

    add file

--- src/app/Component/AllProduct/page.tsx
+++ src/app/Component/AllProduct/page.tsx
@@ -4,11 +4,87 @@ import { Footer1 } from "../Footer/page";
 import Image from "next/image";
 import { useState } from "react";
 import { FaCaretDown } from "react-icons/fa";
+import Link from "next/link";
 
 interface FilterOption {
   name: string;
   options: string[];
 }
+
+const listings = [
+  {
+    image: "/assets/img/chair.jpg",
+    alt: "A modern black chair with wooden legs",
+    title: "The Dandy chair",
+    price: "£250",
+  },
+  {
+    image: "/assets/img/vaseset.jpg",
+    alt: "A set of rustic vases on a pedestal",
+    title: "Rustic Vase Set",
+    price: "£155",
+  },
+  {
+    image: "/assets/img/vase.jpg",
+    alt: "A single sleek vase on a white surface",
+    title: "The Silky Vase",
+    price: "£125",
+  },
+  {
+    image: "/assets/img/lamp.jpg",
+    alt: "A modern hanging lamp with a blue background",
+    title: "The Lucy Lamp",
+    price: "£399",
+  },
+  {
+    image: "/assets/img/modernlamp.jpg",
+    alt: "A modern black chair with wooden legs",
+    title: "The Dandy chair",
+    price: "£250",
+  },
+  {
+    image: "/assets/img/vaseyellow.jpg",
+    alt: "A set of rustic vases on a pedestal",
+    title: "Rustic Vase Set",
+    price: "£155",
+  },
+  {
+    image: "/assets/img/table.jpg",
+    alt: "A single sleek vase on a white surface",
+    title: "The Silky Vase",
+    price: "£125",
+  },
+  {
+    image: "/assets/img/chair5.jpg",
+    alt: "A modern hanging lamp with a blue background",
+    title: "The Lucy Lamp",
+    price: "£399",
+  },
+  {
+    image: "/assets/img/chair.jpg",
+    alt: "A modern black chair with wooden legs",
+    title: "The Dandy chair",
+    price: "£250",
+  },
+  {
+    image: "/assets/img/vaseset.jpg",
+    alt: "A set of rustic vases on a pedestal",
+    title: "Rustic Vase Set",
+    price: "£155",
+  },
+  {
+    image: "/assets/img/vase.jpg",
+    alt: "A single sleek vase on a white surface",
+    title: "The Silky Vase",
+    price: "£125",
+  },
+  {
+    image: "/assets/img/lamp.jpg",
+    alt: "A modern hanging lamp with a blue background",
+    title: "The Lucy Lamp",
+    price: "£399",
+  },
+];
 
 const AllProduct = () => {
   const [openDropdown, setOpenDropdown] = useState<string | null>(null);
@@ -73,10 +149,10 @@ const AllProduct = () => {
             </div>
           ))}
         </div>
-        <div className="flex gap-8">
+        <div className="flex gap-5">
           <div className="relative flex md:hidden">
             <div
-              className="flex items-center space-x-1 cursor-pointer bg-gray-100 md:bg-transparent py-4 px-10 md:py-0 md:px-0"
+              className="flex items-center space-x-1 cursor-pointer bg-gray-100 md:bg-transparent py-4 px-6 md:py-0 md:px-0"
               onClick={() => toggleDropdown("Filter")}
             >
               <span className="text-gray-800 pr-2">Filter</span>
@@ -103,7 +179,7 @@ const AllProduct = () => {
 
           <div className="relative flex">
             <div
-              className="flex items-center space-x-1 cursor-pointer bg-gray-100 md:bg-transparent py-4 px-10 md:py-0 md:px-0"
+              className="flex items-center space-x-1 cursor-pointer bg-gray-100 md:bg-transparent py-4 px-6 md:py-0 md:px-0"
               onClick={() => toggleDropdown("Sorting")}
             >
               <span className="text-gray-800">Sorting by:</span>
@@ -135,6 +211,38 @@ const AllProduct = () => {
               </div>
             )}
           </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8 py-14">
+          {listings.map((listing, index) => (
+            <Link href={"/Component/ProductListing"}>
+              <div key={index} className="pb-3">
+                <div className="w-full h-[300px]">
+                  <Image
+                    src={listing.image}
+                    alt={listing.alt}
+                    width={300}
+                    height={400}
+                    quality={100}
+                    priority
+                    className="object-cover h-full w-full"
+                  />
+                </div>
+                <h2 className="text-lg font-normal text-gray-900 mt-4">
+                  {listing.title}
+                </h2>
+                <p className="text-gray-500">{listing.price}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+
+        <div className="flex justify-center text-center mt-10 mb-10">
+          <button className="bg-gray-100 lg:block text-gray-500 font-light py-4 px-6 w-full lg:w-44">
+            View collection
+          </button>
         </div>
       </div>
       <Footer1 />
