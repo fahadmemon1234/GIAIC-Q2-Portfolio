d447bd73a5a905599ab639b358ebc78458839d96
fahadmemon1234 <fahadmemon131@gmail.com>
2024-12-08 20:06:04 +0500

    add file

---
+++ src/app/Component/About/page.tsx
@@ -0,0 +1,13 @@
+import TopBar from "../topBar/page";
+import { Navbar3 } from "../Navbar/page";
+
+const About = () => {
+  return (
+    <>
+      <TopBar />
+      <Navbar3 />
+    </>
+  );
+};
+
+export default About;
