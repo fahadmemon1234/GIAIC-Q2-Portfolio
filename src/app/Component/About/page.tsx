3e9dc3d287481c765b9f4a87fcd4572a2f2c7e94
fahadmemon1234 <fahadmemon131@gmail.com>
2024-12-08 21:22:36 +0500

    add file

--- src/app/Component/About/page.tsx
+++ src/app/Component/About/page.tsx
@@ -1,11 +1,66 @@
 import TopBar from "../topBar/page";
 import { Navbar3 } from "../Navbar/page";
+import Image from "next/image";
+import Link from "next/link";
+import { Features3 } from "../Features/page";
 
 const About = () => {
   return (
     <>
       <TopBar />
       <Navbar3 />
+
+      <div className="py-16 container flex flex-col md:flex-row items-center justify-center mx-auto">
+        <div className="md:w-9/12 md:text-left">
+          <p className="text-2xl md:text-3xl font-helvetica text-gray-800">
+            A brand built on the love of craftsmanship,
+            <br />
+            quality and outstanding customer service
+          </p>
+        </div>
+        <div className="mt-6 md:mt-0 w-full md:w-2/12 text-center md:text-left">
+          <button className="bg-gray-100 lg:block text-gray-500 font-light py-4 px-2 w-full md:w-22">
+            View our products
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-col container md:flex-row mx-auto lg:py-16 gap-4">
+        <div className="bg-[#2E294E] text-white md:h-[575px] p-14 flex flex-col justify-center md:w-1/2">
+          <h1
+            className="text-2xl md:text-3xl font-helvetica mb-10 md:pt-[20px]"
+            style={{ lineHeight: "40px" }}
+          >
+            It started with a small idea
+          </h1>
+
+          <p className="font-light lg:w-[80%]">
+            A global brand with local beginnings, our story in a <br />
+            small studio in South London in early 2014
+          </p>
+          <div className="md:pt-64 mt-10">
+            <Link href={"/Component/ProductListing"}>
+              <button className="bg-[#4A4E69] lg:block text-white font-light py-4 px-6 w-full lg:w-44">
+                View collection
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center md:w-1/2">
+          <Image
+            src="/assets/img/chair2.jpg"
+            alt="A modern black chair with wooden legs"
+            className="md:h-[575px] h-[360px] object-cover md:block"
+            width={800}
+            height={500}
+            quality={100}
+            priority
+          />
+        </div>
+      </div>
+
+      <Features3 />
     </>
   );
 };
--- src/app/Component/Navbar/page.tsx
+++ src/app/Component/Navbar/page.tsx
@@ -4,6 +4,7 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import { IoCartOutline } from "react-icons/io5";
 import { HiOutlineMenuAlt3, HiX } from "react-icons/hi";
+import Link from "next/link";
 
 export const Navbar1 = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -211,96 +212,90 @@ export const Navbar2 = () => {
 };
 
 export const Navbar3 = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   return (
     <>
-      <nav className="bg-[#FFFFFF] hidden sm:block">
+      <nav className="bg-[#FFFFFF] sm:block">
         <div className="flex items-center justify-between px-6 py-5">
-          <div className="text-gray-600">
-            <IoIosSearch className="text-xl" />
-          </div>
-
           <h1 className="text-2xl font-helvetica text-gray-800">Avion</h1>
 
           <div className="flex items-center space-x-4 text-gray-600">
-            <IoCartOutline className="text-xl" />
-            <FaRegUserCircle className="text-xl" />
-          </div>
-        </div>
-        <hr className="m-auto w-[97%]" />
+            <div className="justify-center space-x-10 text-sm text-gray-500 pr-10 hidden md:flex">
+              <Link href="/Component/About" className="hover:text-gray-800">
+                About us
+              </Link>
+              <a href="#" className="hover:text-gray-800">
+                Contact
+              </a>
+              <a href="#" className="hover:text-gray-800">
+                Blog
+              </a>
+            </div>
+            <IoIosSearch className="text-xl hidden md:block" />
+            <IoCartOutline className="text-xl hidden md:block" />
+            <FaRegUserCircle className="text-xl hidden md:block" />
 
-        <div className="flex justify-center space-x-12 py-5 text-sm text-gray-500">
-          <a href="#" className="hover:text-gray-800">
-            Plant pots
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Ceramics
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Tables
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Chairs
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Crockery
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Tableware
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Cutlery
-          </a>
+            <div className="md:hidden block">
+              <button>
+                <HiOutlineMenuAlt3 className="text-xl" />
+              </button>
+            </div>
+          </div>
         </div>
       </nav>
 
-      <nav className="bg-[#FFFFFF] block sm:hidden font-Poppins">
-        <div className="flex items-center justify-between px-4 py-4">
-          <h1 className="text-2xl font-helvetica text-gray-800">Avion</h1>
+      <div className="flex container md:hidden justify-start md:justify-center bg-[#f9f9f9] space-x-12 py-5 text-sm text-gray-500 overflow-x-auto">
+        <a href="#" className="hover:text-gray-800">
+          All products
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Plant pots
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Ceramics
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Tables
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Chairs
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Crockery
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Tableware
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Cutlery
+        </a>
+      </div>
 
-          <div className="flex items-center space-x-4 text-gray-600">
-            <IoIosSearch className="text-xl" />
-            <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-              {isMenuOpen ? (
-                <HiX className="text-2xl text-gray-800" />
-              ) : (
-                <HiOutlineMenuAlt3 className="text-2xl text-gray-800" />
-              )}
-            </button>
-          </div>
-        </div>
-
-        <hr className="m-auto w-[97%]" />
-
-        <div
-          className={`${
-            isMenuOpen ? "block" : "hidden"
-          } flex flex-col justify-center items-center space-y-4 py-5 text-sm text-gray-500`}
-        >
-          <a href="#" className="hover:text-gray-800">
-            Plant pots
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Ceramics
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Tables
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Chairs
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Crockery
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Tableware
-          </a>
-          <a href="#" className="hover:text-gray-800">
-            Cutlery
-          </a>
-        </div>
-      </nav>
+      <div className="hidden md:flex justify-start md:justify-center bg-[#f9f9f9] space-x-12 py-5 text-sm text-gray-500 overflow-x-auto">
+        <a href="#" className="hover:text-gray-800">
+          All products
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Plant pots
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Ceramics
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Tables
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Chairs
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Crockery
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Tableware
+        </a>
+        <a href="#" className="hover:text-gray-800">
+          Cutlery
+        </a>
+      </div>
     </>
   );
 };
