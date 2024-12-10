e080cc7e0761d8a1b2d9aee070abf05fe7ec5e9c
fahadmemon1234 <fahadmemon131@gmail.com>
2024-12-10 22:19:51 +0500

    add file

--- src/app/Component/Account/page.tsx
+++ src/app/Component/Account/page.tsx
@@ -11,12 +11,20 @@ const LoginPage = ({ toggleForm }: { toggleForm: () => void }) => {
   const [theme, setTheme] = useState<string>("light");
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      const savedTheme = localStorage.getItem("theme");
-      setTheme(savedTheme === "dark" ? "dark" : "light");
-    }, 100);
+    const savedTheme = localStorage.getItem("theme");
+    setTheme(savedTheme === "dark" ? "dark" : "light");
 
-    return () => clearInterval(interval);
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === "theme") {
+        setTheme(event.newValue === "dark" ? "dark" : "light");
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
   }, []);
 
   const [email, setEmail] = useState<string>("");
