9870b82a22749406f2f5c8b5091f06ece067bdcc
fahadmemon1234 <fahadmemon131@gmail.com>
2024-12-11 22:10:54 +0500

    add file

--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -1,6 +1,6 @@
-import sanityClient from "@sanity/client";
+import { createClient } from "@sanity/client";
 
-export const client = sanityClient({
+export const client = createClient({
   projectId: "bwqiebq9",
   dataset: "production",
   apiVersion: "2024-11-01",
