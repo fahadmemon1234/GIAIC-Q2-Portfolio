1e0674322fc03c984f3842e71cde0cbb66584601
fahadmemon1234 <fahadmemon131@gmail.com>
2025-01-22 12:56:59 +0500

    add file

--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -1,10 +1,9 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "j2g8ddia",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2024-11-01",
   useCdn: true,
-  token:
-    "sk0FQNN5I754HJdcXGbmcRwHtfFTvkF9wz3DlDK63koGZkFgIc2iPlAxDK1CVgHhicsLskgnO53wSzBjboby6PxnbH0D19jN9zTcyc1DazdIrSZRkcExnmjny75wnit3EerEe0sQagtiCRe16tHPOwqEHEhWF3WiOMomz3PHyQVntoXr3Sih",
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
